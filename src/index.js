125d08fab05b74ee550b55eb5555f5f599cedd7c
Andre Barros <andrebarros7201@gmail.com>
2024-05-16 20:38:36 +0100

    add buttons to navbar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Test");
+const divContent = document.querySelector("#content");
+
+const title = document.createElement("h1");
+title.classList.add("page-title");
+divContent.appendChild(title);
+
+const nav = document.querySelector("nav");
+nav.classList.add("navbar");
+
+const dropdownButton = document.createElement("button");
+dropdownButton.textContent = "Dropdown Example";
+dropdownButton.classList.add("navbar-button");
+
+nav.appendChild(dropdownButton);
+
+const carouselButton = document.createElement("button");
+carouselButton.textContent = "Carousel";
+carouselButton.classList.add("navbar-button");
+
+nav.appendChild(carouselButton);
